edb53bbd97cfecbb01fddb3dcb39bd70bfd79561
Santiago <jsantiagoh@gmail.com>
2016-01-26 11:12:38 +0100

    minor code deduplication

--- app.js
+++ app.js
@@ -4,32 +4,33 @@ const wit = require('node-wit')
 const dateFormat = require('dateformat')
 
 const ACCESS_TOKEN = process.env.WIT_ACCESS_TOKEN
-if ( !ACCESS_TOKEN ) {
-    console.error("Missing 'WIT_ACCESS_TOKEN' environment variable")
-    process.exit(1)
+if (!ACCESS_TOKEN) {
+  console.error("Missing 'WIT_ACCESS_TOKEN' environment variable")
+  process.exit(1)
 }
 
-function witResponder(res, label, startedAt) {
-    return function (err, witres) {
-      if (err) {
-        res.status(500).send(err)
-      } else {
-        res.json(witres)
-      }
-      const ts = dateFormat(new Date(),"dd-mm-yyyy hh:mm:ss.ms")
-      console.log(`${ts} ${label}: ${(Date.now() - startedAt)} ms` )
+function witResponder(res, label) {
+  const startedAt = Date.now()
+  return function(err, witres) {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.json(witres)
     }
+    const ts = dateFormat(new Date(), "dd-mm-yyyy hh:mm:ss.ms")
+    console.log(`${ts} ${label}: ${(Date.now() - startedAt)} ms`)
+  }
 }
 
 app.get('/v1/text/', function(req, res) {
   const q = req.query.q
-  wit.captureTextIntent(ACCESS_TOKEN, q, witResponder(res, "/text", Date.now()))
+  wit.captureTextIntent(ACCESS_TOKEN, q, witResponder(res, "/text"))
 })
 
 app.post('/v1/speech/', function(req, res) {
-  wit.captureSpeechIntent(ACCESS_TOKEN, req, "audio/wav", witResponder(res, "/speech", Date.now()))
+  wit.captureSpeechIntent(ACCESS_TOKEN, req, "audio/wav", witResponder(res, "/speech"))
 })
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('listening on port 3000.')
 })
